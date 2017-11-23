c3f6bf05179b47e2a54b9c4d624fbb13731798af
Alex Butterworth <abutterworth0912@live.co.uk>
2017-11-23 09:01:20 +0000

    db config

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-	dbURL : 'mongodb://geogaddialex:Kaka1209@ds117156.mlab.com:17156/recycle',
+	dbURL : 'mongodb://admin:th3p455w0rd15@ds117156.mlab.com:17156/recycle',
     // dbURL : 'mongodb://localhost/recycle', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     port : process.env.PORT || 8080
     
