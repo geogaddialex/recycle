463be033c916d35ea2390325167fd604a7b158b5
Alex Butterworth <abutterworth0912@live.co.uk>
2017-11-23 15:22:55 +0000

    route update

--- server/config/express.js
+++ server/config/express.js
@@ -41,7 +41,7 @@
 	app.use('/api/items', itemRoutes);
 	app.use('/api/users', userRoutes);
 
-	app.use('*', express.static('/'));
+	app.use('*', '/');
 
 	app.use(function(req, res, next) {
 	  var err = new Error('Not Found');
