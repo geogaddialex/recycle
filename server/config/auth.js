acd09fb16953b3293cf8d50e17386116382e2311
Alex Butterworth <abutterworth0912@live.co.uk>
2018-03-01 02:40:22 +0000

    facebook heroku test

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,10 +1,9 @@
-// expose our config directly to our application using module.exports
 module.exports = {
 
     'facebookAuth' : {
         'clientID'      : '1827883733922990',
         'clientSecret'  : '83b3a2a056200b5a7632c0b10c476d4a',
-        'callbackURL'   : '/api/auth/facebook/callback',
+        'callbackURL'   : '/api/auth/facebook/callback/',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     },
